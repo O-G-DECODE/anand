1063ba0de20b82933ad4d238b47df49066195187
O-G-DECODE <anandog12@gmail.com>
2024-10-05 12:46:25 +0530

    connectionchqnges

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 // Establish a connection to the MySQL database
-$conn = mysqli_connect("localhost", "root", "", "mydb");
+$conn = mysqli_connect("localhost", "root", "", "mydatabase");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
